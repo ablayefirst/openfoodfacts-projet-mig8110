6f16de15f628fa92cc42f06244673798ab73c908
takfarinas625 <takfarinas.boundaoui@gmail.com>
2026-02-19 12:32:19 +0100

    suppression de en: et fr

--- data/code/6_nettoyage final/en.py
+++ data/code/6_nettoyage final/en.py
@@ -1,26 +1,27 @@
 import pandas as pd
 
-df = pd.read_csv("../../dataset/brut/nettoyage/netoyer1.csv")
+df = pd.read_csv("../../gold/nettoye.csv")
 
 for col in df.columns:
     if df[col].dtype == 'object':
         df[col] = (
             df[col]
             .fillna('')
-            .str.replace(r'en:', '', regex=True)
+            .str.lower()
+            .str.replace('fr:', '', regex=False)
+            .str.replace(' fr:', '', regex=False)
+            .str.replace(',fr:', ',', regex=False)
+            .str.replace('en:', '', regex=False)
             .str.strip()
         )
         df[col] = df[col].replace('', 'unknow')
 
 
-
-# 🔥 Supprime toutes les colonnes dont le nom commence par "Unnamed"
+# Supprimer colonnes Unnamed
 df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
-
-
 
 print("Colonnes Unnamed supprimées et fichier nettoyé ✅")
 
+df.to_csv("../../gold/nettoye.csv", index=False)
 
-df.to_csv("../../dataset/brut/nettoyage/netoyer1.csv", index=False)
 print("Nettoyage terminé")
