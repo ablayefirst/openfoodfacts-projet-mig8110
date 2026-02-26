29abd552f968e459590b92dc4234e958b2fe2aaf
Abdoulaye Aguibou Bah <je791966@ens.uqam.ca>
2026-02-25 22:13:28 -0500

    Mise a jour de app/db.py

--- app/db.py
+++ app/db.py
@@ -1,9 +1,10 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker  
+from sqlalchemy.orm import sessionmaker
+
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg2://postgres:admin@localhost:5432/openfoodfacts_canada "
+    "postgresql+psycopg2://postgres:admin@localhost:5432/openfoodfacts_canada"
 )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
